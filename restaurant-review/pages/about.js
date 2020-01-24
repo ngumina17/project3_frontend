868d2682a381ab401db95eac2931a14a3568b69c
Danny Gleason <danny.c.gleason@gmail.com>
2020-01-24 17:51:08 -0500

    deleted src

---
+++ restaurant-review/pages/about.js
@@ -0,0 +1,7 @@
+
+
+function AboutPage() {
+    return <div>Welcome to Next.js from the About Page</div>
+  }
+  
+  export default AboutPage
---
+++ restaurant-review/pages/index.js
@@ -0,0 +1,9 @@
+
+import AboutPage from "./about"
+
+
+function HomePage() {
+    return <div>Welcome to Next.js! <AboutPage/></div>
+  }
+  
+  export default HomePage
