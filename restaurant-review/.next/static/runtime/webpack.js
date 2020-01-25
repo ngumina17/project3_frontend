e501867c2d9a2cbbdb27d05459796d4ad084366c
KASSIDY_SHADE <kassie2129@gmail.com>
2020-01-25 10:57:43 -0600

    changed a couple links

--- restaurant-review/.next/static/runtime/webpack.js
+++ restaurant-review/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b7666976db366d0e6293";
+/******/ 	var hotCurrentHash = "73d4d04f5d8d00521ba7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
